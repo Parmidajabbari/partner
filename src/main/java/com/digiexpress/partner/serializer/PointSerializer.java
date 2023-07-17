f8049e155eaaa8b7919dcae8d069ee520898f13e
Parmidajabbari <parmidajabbari@gmail.com>
2023-07-17 12:05:25 +0330

    implement serializer methods

---
+++ src/main/java/com/digiexpress/partner/serializer/PointSerializer.java
@@ -0,0 +1,22 @@
+package com.digiexpress.partner.serializer;
+
+import com.fasterxml.jackson.core.JsonGenerator;
+import com.fasterxml.jackson.databind.JsonSerializer;
+import com.fasterxml.jackson.databind.SerializerProvider;
+import org.springframework.data.mongodb.core.geo.GeoJsonPoint;
+
+import java.io.IOException;
+
+public class PointSerializer extends JsonSerializer<GeoJsonPoint> {
+    @Override
+    public void serialize(GeoJsonPoint value, JsonGenerator gen, SerializerProvider serializers) throws IOException {
+        gen.writeStartObject();
+        gen.writeStringField("type", value.getType());
+
+        gen.writeArrayFieldStart("coordinates");
+        gen.writeObject(value.getX());
+        gen.writeObject(value.getY());
+        gen.writeEndArray();
+        gen.writeEndObject();
+    }
+}
